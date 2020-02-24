98eaac0d8416e8d8266ed84c005c7be474166710
Brayan <Brayan@DESKTOP-917QF96>
2020-02-24 01:24:59 -0500

    Primera Subida test

---
+++ ERPSedisa.Trabajos.API/ERPSedisa.Trabajos.Entidades/Trabajos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERPSedisa.Trabajos.Entidades
+{
+    public class Trabajos
+    {
+        public int IdTrabajo { get; set; }
+        public int IdTipotrabajo { get; set; }
+        public string Trabajo { get; set; }
+        public int IdEstado { get; set; }
+        public string Observacion { get; set; }
+        public DateTime Fecha_Registro { get; set; }
+        public int Aud_UsuarioRegistro { get; set; }
+        public DateTime Aud_FechaRegistro { get; set; }
+        public int Aud_UsuarioModifica { get; set; }
+        public DateTime Aud_FechaModifica { get; set; }
+        public int Bit_Activo { get; set; }
+    }
+}
